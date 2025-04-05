15bda8c3ccb995263f5f20dea6a1ac82dc9936c5
bigdethan <m.offender@yahoo.com>
2025-04-05 18:09:08 +0800

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyD8BFy8S6d7y9ZtW8cs65VI-m6oC9ge61c",
+  authDomain: "crud-cuadra.firebaseapp.com",
+  databaseURL: "https://crud-cuadra-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "crud-cuadra",
+  storageBucket: "crud-cuadra.firebasestorage.app",
+  messagingSenderId: "449063502387",
+  appId: "1:449063502387:web:12ed2a9a1c14bd45749946"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
